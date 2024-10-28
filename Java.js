6aa5679524371706f9433d7f1c2299268894168a
EsopDev <esopsamuel47@gmail.com>
2024-10-28 06:41:07 -0700

    first commit

---
+++ Java.js
@@ -0,0 +1,12 @@
+// alert('Welcome to Javascript Class')
+// document.write('welcome to Javascript Class')
+window.confirm('Do you Wanna Delete me?');
+
+// Syntax used to set a variables
+// let
+// const
+// var
+
+let mynamE =40;
+let myname =10;
+console.log(++myname);
